68e054eb82fa526a23416cd4c87927f56f64c190
LouWilly <mllf.flor@gmail.com>
2022-06-20 09:03:16 -0300

    a

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{( str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" contents="width=device-width, initial-scale=1">
+        <title>Bootstrap Laravel</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}"></script>
+        <script src="{{asset('js/jquery.js')}}"></script>
+        <script src="{{asset('js/bootstrap.js')}}"></script>
+</head>
+<body>
+    <h1>Utilizando Bootstrap</h1>
+    <button class="btn btn-lg btn-success">Bootstrap</button>
+</body>
+</html>
